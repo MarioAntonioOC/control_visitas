3e65159e0ac5a451235d17b22b80b4b0c77297de
Mario_AntonioOC <oiram90140@gmail.com>
2023-04-21 15:58:52 -0500

    first commit

---
+++ src/main/java/com/utrng/Control_Visitas_Biblioteca/ControlVisitasBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.utrng.Control_Visitas_Biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlVisitasBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlVisitasBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utrng/Control_Visitas_Biblioteca/ControlVisitasBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utrng.Control_Visitas_Biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlVisitasBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
